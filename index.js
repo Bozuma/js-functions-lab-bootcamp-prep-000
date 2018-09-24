d3326205e3ebc4aa425db22c13a00459e0a5a6ff
Bozuma Acquaah <bozuma.acquaah@gmail.com>
2018-09-24 01:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ var holiday = 'Independence Day'
 function happyHolidays(){
   return “Happy holidays!”;
 }
-happyHolidays();
+
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo();
+
 
 function happyHolidayTo(holiday, name){
 return `Happy ${holiday}, ${name}!`;
 }
-happyHolidayTo();
+
 
 function holidayCountdown(mother, number){
 return `It’s ${number} days until ${mother}!`;
 }
-holidayCountdown();
