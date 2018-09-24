40ce2d98003d348b014e88a9f4c0e6b15cf8d80b
Bozuma Acquaah <bozuma.acquaah@gmail.com>
2018-09-24 00:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+var name, holiday, day
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo() {
+  return "Happy Holidays,"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+}
+
+
+function holidayCountdown(day, holiday){
+  return 'It/s ${days} days until ${holiday}!'
+}
